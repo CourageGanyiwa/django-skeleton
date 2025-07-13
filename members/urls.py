ce43d8487bc8bc897bdfd6d29dd4270da18a9796
Courage Ganyiwa <couresma@gmail.com>
2025-07-13 14:27:26 +0200

    latest changes

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+urlpatterns = [ path('', views.main , name='main'),
     path('members', views.members, name='members'),
     path('details/<int:id>', views.details, name='details'),
 ]
