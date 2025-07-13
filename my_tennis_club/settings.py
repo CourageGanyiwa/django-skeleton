52b5eba684cc28410fca97df203c11f20f95a968
Courage Ganyiwa <couresma@gmail.com>
2025-07-13 17:25:09 +0200

    latest changes

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5+o-@@9sxm#nc%q9&$7l-gy++%=l(ol=nvtw6s#rogya=%8u1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
