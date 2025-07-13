b120fccb01ce1de4500488a4719d23ba18ffee92
Courage Ganyiwa <couresma@gmail.com>
2025-07-13 20:47:35 +0200

    installed whitenoise for static files

--- members/views.py
+++ members/views.py
@@ -23,3 +23,11 @@ def details(request, id):
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+def testing(request):
+    mydata = Member.objects.all().values('id','first_name', 'last_name', 'phone', 'joined_date')
+    template = loader.get_template('template.html')    
+    context = {
+        'mymembers': mydata,
+    }
+    return HttpResponse(template.render(context,request))
